d00a70af94f11dd8445f6e88b25731b061a9ba3b
Henry Schreiner <henryschreineriii@gmail.com>
2020-09-14 14:18:11 -0400

    feat: init

---
+++ pybind11_mkdoc/__init__.py
@@ -0,0 +1,16 @@
+"""
+This is a package for building pybind11 docstrings from C++ header comments.
+
+(Docs WIP).
+"""
+
+
+import sys
+
+from .mkdoc_lib import mkdoc
+
+
+__version__ = "2.6.0.dev1"
+
+def main(self):
+    return mkdoc(sys.argv[:1])
