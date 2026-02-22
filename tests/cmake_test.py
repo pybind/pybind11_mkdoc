8b70ad0a3c9b1b04beb3e788edffee3230c3458a
leakec <leakec57@gmail.com>
2026-02-22 09:12:45 -0700

    Trying to pass in Python version directly to help cmake in the runners.

--- tests/cmake_test.py
+++ tests/cmake_test.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import subprocess
 from pathlib import Path
 
@@ -8,7 +9,7 @@ DIR = Path(__file__).resolve().parent
 def test_pybind11_mkdoc_cmake(tmp_path: Path) -> None:
     # Run pybind11-mkdoc and put the output in a temp file
     build_dir = tmp_path / "build"
-    subprocess.run(["cmake", "-B", build_dir, "-S", DIR / "cmake_docs"], check=True)
+    subprocess.run(["cmake", "-B", build_dir, "-S", DIR / "cmake_docs", f"-DPython_EXECUTABLE={sys.executable}"], check=True)
     subprocess.run(["cmake", "--build", build_dir], check=True)
 
     # Ensure the header file matches
@@ -26,7 +27,7 @@ def test_pybind11_mkdoc_cmake_extra_args(tmp_path: Path) -> None:
     env = os.environ.copy()
     env["PYBIND11_TEST_EXTRA_ARGS"] = "-DMY_EXTRA_DEFINE=1"
 
-    subprocess.run(["cmake", "-B", build_dir, "-S", DIR / "cmake_docs"], check=True, env=env)
+    subprocess.run(["cmake", "-B", build_dir, "-S", DIR / "cmake_docs", f"-DPython_EXECUTABLE={sys.executable}"], check=True, env=env)
     subprocess.run(["cmake", "--build", build_dir], check=True, env=env)
 
     # Ensure the header file matches
